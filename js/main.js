edd9ee7dcc74b03f36c46404a955230ca5bf0db0
Syrikens <caialeksander@gmail.com>
2023-06-07 19:18:01 +0200

    module2-task1
    Написаны функции

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+let randomizerFunc = function (min, max) {
+        if (max <= min) {
+        alert("Значение max, должно привышать значение min")
+    } else {
+    let rand = Math.floor(min + Math.random() * (max + 1 - min));
+    alert(rand)
+}
+}
 
+randomizerFunc(20, 25)
+
+let timingCheck = function (checkОbject, maxObjectLength){
+    numberOfChars = checkОbject.length
+    if (numberOfChars <= maxObjectLength) {
+        alert ("true")
+    }
+        else  (numberOfChars > maxObjectLength){
+            alert (false)
+        }
+    }
+    timingCheck("Привет", 2)
